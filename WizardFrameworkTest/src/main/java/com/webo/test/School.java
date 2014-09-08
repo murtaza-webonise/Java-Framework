545dcdf2a761753a75ce48eff220def519343e77
Murtaza Bagwala <murtaza.bagwala@weboniselab.com>
2014-09-08 14:47:51 +0530

    updated

--- WizardFrameworkTest/src/main/java/com/webo/test/School.java
+++ WizardFrameworkTest/src/main/java/com/webo/test/School.java
@@ -1,20 +1,25 @@
 package com.webo.test;
 
 import com.webo.annotations.Scope;
-
 @Scope
 public class School {
-	private String schoolName = "A.B.N";
-	public  School() {
+	private String schoolName;
+
+	public School() {
 		System.out.println("School");
 	}
 
-	public String getSchoolName() {
-		return schoolName;
+	public void printSchoolName() {
+		System.out.println(schoolName);
 	}
 
 	public void setSchoolName(String schoolName) {
 		this.schoolName = schoolName;
+	}
+
+	public String getSchoolName() {
+		// TODO Auto-generated method stub
+		return schoolName;
 	}
 
 }
--- WizardFrameworkTest/src/test/java/Tester.java
+++ WizardFrameworkTest/src/test/java/Tester.java
@@ -1,12 +1,21 @@
 
+import com.webo.test.School;
+import com.webo.wizard.config.BeanFactory;
+import com.webo.wizard.config.PropertyParser;
+
 public class Tester {
 
-	
 	public static void main(String[] args) {
-		
-		PropertyParser pp=new PropertyParser();
+
+		PropertyParser pp = new PropertyParser();
 		pp.parser("/home/webonise/workspace/WizardFrameworkTest/src/main/resources/properties.json");
+		School sc = (School) BeanFactory.retrFromMap("school");
+		sc.setSchoolName("I.L.V.A");
+		System.out.println(sc.getSchoolName());
+		School sc1 = (School) BeanFactory.retrFromMap("school");
+		System.out.println(sc1.getSchoolName());
 		
+
 	}
 
 }
